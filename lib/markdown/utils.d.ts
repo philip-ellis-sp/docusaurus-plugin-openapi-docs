97f436d7433891365e758b92f274b03ca1e2738b
philip-ellis-sp <philip.ellis@sailpoint.com>
2022-08-29 17:16:39 -0400

    added lib files

---
+++ lib/markdown/utils.d.ts
@@ -0,0 +1,7 @@
+export declare type Children = string | undefined | (string | string[] | undefined)[];
+export declare type Props = Record<string, any> & {
+    children?: Children;
+};
+export declare function create(tag: string, props: Props): string;
+export declare function guard<T>(value: T | undefined, cb: (value: T) => Children): string;
+export declare function render(children: Children): string;
